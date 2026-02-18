125d54719ac0a18c395fe9c6336ef7c3f445ae82
VondeT <s979083@student.knoxschools.org>
2026-02-18 15:28:42 +0000

    Dictionary

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,2 +1,10 @@
 #⭐⭐⭐⭐ Create a program that tracks student names and test scores.
 print(hello)
+
+student_info = {
+    "Niko" : 97,
+    "Rylee" : 55,
+    "Avery" : 80,
+    "Gavin" : 75
+    }
+
