1db8d2bab72adf9cbddcc688d8893610621ef3d3
LiamA-Git <s977903@student.knoxschools.org>
2026-02-18 15:38:20 +0000

    accepted change

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #⭐⭐⭐⭐ Create a program that tracks student names and test scores.
 print(hello)
 # Dictionary for students and student grades
@@ -8,6 +7,4 @@ student_info = {
     "Avery" : 80,
     "Gavin" : 75
     }
-=======
->>>>>>> ae2f256 (kugv)
 
